f825f376bfaf6cec3f117397906ed7619bed7939
Jack Maders <jackwmaders@gmail.com>
2024-01-22 20:30:50 +0000

    add prettier and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@nuxtjs/eslint-module"],
+  devtools: { enabled: true },
+  eslint: {
+    // prevent warning on startup
+    lintOnStart: false,
+  },
+});
